d53a07046da43933c1336e9a4bca52e0ff4487a2
baialei <850568054@qq.com>
2025-04-04 10:32:29 +0800

    1

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,7 +3,7 @@ import {defineUserConfig} from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-    base: "/",
+    base: "/ispace-website/",
     lang: "zh-CN",
     title: "Golang 中文学习文档",
     description: "Go 爱好者驱动的中文学习文档",
