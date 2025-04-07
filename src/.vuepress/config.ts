e5eae5d7af359433616d053412b81f48e697d210
baialei <850568054@qq.com>
2025-04-07 09:15:58 +0800

    添加百度统计

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,34 +7,20 @@ export default defineUserConfig({
     lang: "zh-CN",
     title: "ISpace",
     description: "我的专属空间",
+ 
+
 
     head: [
         [
             "script",
             {},
-            `var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?000";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();`,
-        ],
-        [
-            "script",
-            {
-                async: true,
-                src: "https://www.googletagmanager.com/gtag/js?id=000"
-            }
-        ],
-        [
-            "script",
-            {},
-            `window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-000');`
+            `    var _hmt = _hmt || [];
+    (function() {
+      var hm = document.createElement("script");
+      hm.src = "https://hm.baidu.com/hm.js?548f817bec23cce94292d5ef840836dc";
+      var s = document.getElementsByTagName("script")[0]; 
+      s.parentNode.insertBefore(hm, s);
+    })();`,
         ],
     ],
 
