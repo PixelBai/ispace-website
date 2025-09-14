e747f4ebc70fd4e510876592db3790babcdf0307
baialei <850568054@qq.com>
2025-09-14 22:00:31 +0800

    初步完成文档

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -11,12 +11,7 @@ export const zhSidebar = sidebar({
       icon: "activity",
       text: "使用",
       link: "use",
-    },
-    {
-      icon: "software",
-      text: "开发",
-      link: "develop", 
-    },
+    }, 
     {
       icon: "activity",
       text: "部署",
