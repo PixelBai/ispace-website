fba270ac3290e13eb06090e877038183c328596d
baialei <850568054@qq.com>
2025-10-22 17:47:53 +0800

    完成优化

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -1,7 +1,9 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
-  { text: "快速开始", icon: "alias", link: "/quickstart",activeMatch: "/quickstart" },
+  { text: "首页", icon: "home", link: "/", activeMatch: "" },
+  { text: "快速开始", icon: "guide", link: "/quickstart",activeMatch: "/quickstart" },
   { text: "文档", icon: "alias", link: "/docs/overview",activeMatch: "/docs/" },
-  { text: "关于我", icon: "alias", link: "/about/forme", activeMatch: "/about/" },
+  { text: "关于我", icon: "info", link: "/about", activeMatch: "/about" },
+  { text: "源码", icon: "gitee", link: "https://gitee.com/baialei/ispace" },
 ]);
